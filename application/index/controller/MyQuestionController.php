14203e5c272d08d80dbaba01688314a4ec7fc0aa
刘嘉诚 <1055112835@qq.com>
2019-02-23 18:22:39 +0800

    主页问题，我的问题，地区列表

--- application/index/controller/MyQuestionController.php
+++ application/index/controller/MyQuestionController.php
@@ -15,7 +15,8 @@ class MyQuestionController extends Base
 {
 
     public function my_question_index () {
-        $uid=$this->get_uid();
+        $token = input('token','','trim');
+        $uid=$this->lenovo_getuid($token);
         $res = Db::name('question')->alias('a')->join('admin b','a.admin_id=b.id')->where(['is_deleted'=>0,'id'=>$uid])->order('show_number')->paginate(15);
         if ($res) {
             return $this->output_success(10010,$res,'获取我的问题列表成功');
