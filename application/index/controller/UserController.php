eb77f016bf6bbcd2a8ff85072465f1107a58519c
刘嘉诚 <1055112835@qq.com>
2019-02-23 18:11:22 +0800

    test

---
+++ application/index/controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/2/23
+ * Time: 12:21
+ */
+
+namespace app\index\controller;
+
+
+class Usercontroller extends Base
+{
+    public function index()
+    {
+        echo 'dsfjffd';
+    }
+}
