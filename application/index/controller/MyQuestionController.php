0d78b77b9c32966555b201d066ffe4e006048dc4
刘嘉诚 <1055112835@qq.com>
2019-02-23 21:38:01 +0800

    添加分页

--- application/index/controller/MyQuestionController.php
+++ application/index/controller/MyQuestionController.php
@@ -15,13 +15,25 @@ class MyQuestionController extends Base
 {
 
     public function my_question_index () {
+        $page = input('page',1,'intval');
         $token = input('token','','trim');
         $uid=$this->lenovo_getuid($token);
-        $res = Db::name('question')->alias('a')->join('admin b','a.admin_id=b.id')->where(['is_deleted'=>0,'id'=>$uid])->order('show_number')->paginate(15);
+        $res = Db::name('question')->alias('a')->join('admin b','a.admin_id=b.id')->where(['a.is_deleted'=>0,'a.user_id'=>$uid])->order('a.show_number')->page($page,15)->select();
         if ($res) {
             return $this->output_success(10010,$res,'获取我的问题列表成功');
         } else {
             return $this->output_error(10000,'获取我的问题列表失败');
+        }
+    }
+
+    public function change_status () {
+        $status = input('status',1,'intval');
+        $token = input('token','','trim');
+        $qid = input('qid','','intval');
+        $uid=$this->lenovo_getuid($token);
+        $res = Db::name('question')->where(['user_id'=>$uid,'id'=>$qid])->update(['status'=>$status]);
+        if (!$res) {
+            return $this->output_error(10000,'我的问题修改失败');
         }
     }
 }
--- application/index/controller/QuestionInformationController.php
+++ application/index/controller/QuestionInformationController.php
@@ -15,7 +15,8 @@ use think\exception\DbException;
 class QuestionInformationController extends Base
 {
     public function question_index () {
-        $res = Db::name('question')->alias('a')->join('admin b','a.admin_id=b.id')->where(['is_deleted'=>0])->order('show_number')->paginate(15);
+        $page = input('page',1,'intval');
+        $res = Db::name('question')->alias('a')->join('admin b','a.admin_id=b.id')->where(['a.is_deleted'=>0])->order('a.show_number')->page($page,15)->select();
         if ($res) {
             return $this->output_success(10010,$res,'获取问题列表成功');
         } else {
