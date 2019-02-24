19f619501cd3f30ac387fc8d6e7b8cda2981749b
刘嘉诚 <1055112835@qq.com>
2019-02-24 19:40:55 +0800

    fix鉴权

--- application/index/controller/AreaController.php
+++ application/index/controller/AreaController.php
@@ -13,8 +13,19 @@ use think\Db;
 
 class AreaController extends Base
 {
+
+    /**
+     * 联想地区获取
+     * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+
     public function index () {
+
         $res = Db::name('area')->where(['is_deleted'=>0])->select();
+
         if ($res) {
             return $this->output_success(10010,$res,'获取地区成功');
         } else {
--- application/index/controller/QuestionInformationController.php
+++ application/index/controller/QuestionInformationController.php
@@ -14,9 +14,21 @@ use think\exception\DbException;
 
 class QuestionInformationController extends Base
 {
+    /**
+     * 主页问题列表
+     * @return array
+     * @throws DbException
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+
     public function question_index () {
+
         $page = input('page',1,'intval');
+//        当前问题页面
+
         $res = Db::name('question')->alias('a')->join('admin b','a.admin_id=b.id')->where(['a.is_deleted'=>0])->order('a.show_number')->page($page,15)->select();
+
         if ($res) {
             return $this->output_success(10010,$res,'获取问题列表成功');
         } else {
