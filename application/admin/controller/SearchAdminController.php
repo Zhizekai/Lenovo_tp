f066a370b149413aea95ee979e7f270f5b9e169a
刘嘉诚 <1055112835@qq.com>
2019-03-02 13:23:11 +0800

    Merge branch 'feat_1st_phase' of code.aliyun.com:WNlenovo/lenovo_tp into feat_1st_phase
    # Conflicts:
    #	application/admin/controller/AdminController.php
    #	application/index/controller/Base.php
    #	application/index/controller/UserController.php

--- application/admin/controller/SearchAdminController.php
+++ application/admin/controller/SearchAdminController.php
@@ -20,15 +20,15 @@ class SearchAdminController extends AdminBase
 
         $this->check_admin(1);
         var_dump('33333');die;
-        $account = input('account','','trim');
-        $status = input('status','','trim');
-
-        $where = ['account'] = ['like','%'.$account.'%'];
-        $where = ['status'] = ['like','%'.$status.'%'];
-
-        $res = Db::name('name')->where()->find();
-
-
-        return $this->output_success(200,$res,'success');
+//        $account = input('account','','trim');
+//        $status = input('status','','trim');
+//
+//        $where = ['account'] = ['like','%'.$account.'%'];
+//        $where = ['status'] = ['like','%'.$status.'%'];
+//
+//        $res = Db::name('name')->where()->find();
+//
+//
+//        return $this->output_success(200,$res,'success');
     }
 }
--- application/index/controller/UserController.php
+++ application/index/controller/UserController.php
@@ -24,7 +24,6 @@ class UserController extends Base
      * @throws \think\exception\PDOException
      */
     public function register()
-
     {
         //接收数据
         $code = input('code',0,'trim');
@@ -73,7 +72,6 @@ class UserController extends Base
 
 
     public function sign_in()
-
     {
         $code = input('code',0,'trim');
 
@@ -82,68 +80,17 @@ class UserController extends Base
         if (array_key_exists('errmsg',$openid)){
             return $this->output_error(40029,'code错误|appkey|appscrect错误');
         }
-
-
-
-
-        $res_openid = Db::name('user')->where('openid',$openid)->value('id');
-        if ($res_openid) {
-            return $this->output_error(400,'该用户已注册，请登录');
-        }
-
-
-        $area_id = Db::name('area')->where(['area'=>$area])->value('id');
-        if (!$area_id) {
-            return $this->output_error(10010,'请输入正确的地理位置');
-        }
-
-
-        $res = Db::name('user')->insert(['years'=>$years,'area_id'=>$area_id,'open_id'=>$openid['openid']]);
 
 
         //更新token和token过期时间
         $token = $this->token_create($openid['openid']);
-        $res = Db::name('user')->where('open_id',$openid['openid'])->update(['token'=>$token,'api_token_expire'=>time()]);
+        $res = Db::name('user')->where('open_id',$openid['openid'])->update(['api_token'=>$token,'api_token_expire'=>time()]);
         if (empty($res)){
             return $this->output_error(40029,'登陆失败');
         }
-
-
-        $token = $this->token_create($openid['openid']);
-
-        return $this->output_success(200,$token,'注册成功');
-
-
-    }
-
-
-    public function sign_in()
-    {
-        $code = input('code',0,'trim');
-
-        //得到openid
-        $openid = $this->get_openid($code);
-        if (array_key_exists('errmsg',$openid)){
-            return $this->output_error(40029,'code错误|appkey|appscrect错误');
-        }
-
-
-        //更新token过期时间
-        //===========
-        $res = Db::name('user')->where('open_id',$openid)->update(['api_token_expire'=>time()]);
-
-        if (empty($res)){
-            return $this->output_error(40029,'登陆失败');
-        }
-
-        $token = $this->token_create($openid);
-
-
-
 
         //返回登陆信息
         return $this->output_success(200,$token,'登陆成功');
-
     }
 
 
@@ -151,13 +98,8 @@ class UserController extends Base
     public function zzk()
     {
 
-        $request = Request::instance()->header();
-        dump($request);
-
-
 
         var_dump(date('Y-m-d',time()));
-
     }
 
     /**
@@ -166,10 +108,6 @@ class UserController extends Base
      * @return mixed
      */
     public function get_openid($code){
-
-
-//wx98cb9fae6e8b0cca
-
 
         $appid = 'wx98cb9fae6e8b0cca';
         $appsecret = 'b215d069d948ea9c79234a25b1eba9ea';
